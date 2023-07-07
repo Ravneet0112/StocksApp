b83fe19304d3db49d3bec66eac89d15f39bf916d
Ravneet Kaur Sammewali <ravneet.sammewali01@gmail.com>
2023-07-07 00:55:45 -0400

    Modified server page

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,18 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+let sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3006,
+  }
+);
+
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
